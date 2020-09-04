69c6f0547c3a3d99ceb8e0b564a14930fdc395b8
munificent-forum-1695 <bunchshawn@gmail.com>
2020-09-04 19:07:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,15 +3,15 @@ katz_deli = []
 other_deli = ["Logan", "Avi", "Spencer"]
 another_deli = ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"]
 # Write your code here.
-def line(array) #define line method referring to katz_deli array
-  if array.length >= 1 #if statment wants to find if there is anything in the array. Then interpolate on each element (number in line and name)
-    new_array = [] #creating a space to store the new output of this method
+def line(deli_array) #define line method referring to katz_deli array
+  if deli_array.length >= 1 #if statment wants to find if there is anything in the array. Then interpolate on each element (number in line and name)
+    storage_array = [] #creating a space to store the new output of this method
     counter = 1 #create counter for each element (>=1) starting at 1.
-    array.each do |guest| #here we interpolate over each element of katz_deli array.
-      new_array.push("#{counter}. #{guest}") #call .push on the new array to fill with interpolated operations
+    deli_array.each do |guest| #here we interpolate over each element of katz_deli array.
+      storage_array.push("#{counter}. #{guest}") #call .push on the new array to fill with interpolated operations
       counter += 1
     end
-    puts "The line is currently: #{new_array.join(" ")}"
+    puts "The line is currently: #{storage_array.join(" ")}"
   else
     puts "The line is currently empty."
   end #if there is no one in the line, meaning the katz_deli array is empty, puts "the line is empty".
